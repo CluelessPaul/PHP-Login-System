6930ec9d0d1522e5a4ce4078d74305f97dd2a14f
Paul <paul.h.rojas@gmail.com>
2019-08-12 00:40:47 -0400

    Added config; added login/reg pages; added footer; simplified index page

---
+++ inc/footer.php
@@ -0,0 +1,6 @@
+<!-- UIkit CSS -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.1.7/css/uikit.min.css" />
+
+<!-- UIkit JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.1.7/js/uikit.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.1.7/js/uikit-icons.min.js"></script>
