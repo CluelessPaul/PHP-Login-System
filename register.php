c883c77864df6d4387366ec84fd45666cdf8018c
Paul <paul.h.rojas@gmail.com>
2019-08-13 01:28:39 -0400

    DB connection created using PDO;

--- register.php
+++ register.php
@@ -15,7 +15,7 @@
 
     <title>Title</title>
 
-    <base href="/" />
+    <base href="/Devfiles/4th_class/" />
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.1.7/css/uikit.min.css" />
 </head>
