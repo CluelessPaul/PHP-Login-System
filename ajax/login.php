5166e5559e567937ced7d03db6f2fac78f4df22d
Paul <paul.h.rojas@gmail.com>
2019-09-18 00:08:06 -0400

    - Login.php corrected - Created class User and made arrangements to include the class User

--- ajax/login.php
+++ ajax/login.php
@@ -1,42 +1,45 @@
 <?php
-    // Allow the config
-    define('__CONFIG__', true);
 
-    // Require the config
-    require_once '../inc/config.php';
+// Allow the config
+define('__CONFIG__', true);
 
-    if($_SERVER['REQUEST_METHOD'] == 'POST') {
-        // Always return JSON format
-        // header('Content-Type: application/json');
+// Require the config
+require_once "../inc/config.php";
 
-        $return = [];
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    // Always return JSON format
+    // header('Content-Type: application/json');
 
-        $email = Filter::String( $_POST['email'] );
-        $password = Filter::String( $_POST['password'] );
-        $user_found = User::Find($email, true);
+    $return = [];
 
-        if($user_found) {
-            // User exists, try and sign them in
-            $user_id = (int) $user_found['user_id'];
-            $hash = (string) $user_found['password'];
+    $email = Filter::String( $_POST['email'] );
+    $password = $_POST['password'];
 
-            if(password_verify($password, $hash)) {
-                // User is signed in
-                $return['redirect'] ='../dashboard.php';
+    $user_found = User::Find($email, true);
 
-                $_SESSION['user_id'] = $user_id;
-            } else {
-                // Invalid user email/password combo
-                $return['error'] = 'Invalid user email/password combo';
-            }
+    if($user_found) {
+        // User exists, try and sign them in
+        $user_id = (int) $user_found['user_id'];
+        $hash = (string) $user_found['password'];
 
+        if(password_verify($password, $hash)) {
+            // User is signed in
+            $return['redirect'] = '/dashboard.php';
+
+            $_SESSION['user_id'] = $user_id;
         } else {
-            // They need to create a new account
-            $return['error'] = "You do not have an account. <a href='./register.php'>Create one now?</a>";
+            // Invalid user email/password combo
+            $return['error'] = "Invalid user email/password combo";
         }
 
-        echo json_encode($return, JSON_PRETTY_PRINT); exit;
     } else {
-        // Die. Kill the script. Redirect the user. Do something regardless
-        exit('Invalid URL');
+        // They need to create a new account
+        $return['error'] = "You do not have an account. <a href='./register.php'>Create one now?</a>";
     }
+
+    echo json_encode($return, JSON_PRETTY_PRINT); exit;
+} else {
+    // Die. Kill the script. Redirect the user. Do something regardless.
+    exit('Invalid URL');
+}
+?>
--- inc/config.php
+++ inc/config.php
@@ -12,5 +12,6 @@
     // Include the DB.php file
     include_once 'classes/DB.php';
     include_once 'classes/Filter.php';
+    include_once 'classes/User.php';
 
     $con = DB::getConnection();
