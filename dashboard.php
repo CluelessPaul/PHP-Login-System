a37ebe5aa525b6743ec3c40271a93df349edb563
Paul Rojas <paul@Pauls-MacBook-Pro.local>
2019-11-25 02:24:27 -0500

    in progress. TODO: fix line 13

--- dashboard.php
+++ dashboard.php
@@ -3,9 +3,22 @@
     define('__CONFIG__', true);
     // Require the config
     require_once 'inc/config.php';
+
     Page::ForceLogin();
 
-    $User = new User($_SESSION['user_id']);
+    $user_id = new User($_SESSION['user_id']);
+
+    $getUserInfo = $con->prepare("SELECT email, reg_time FROM users WHERE user_id = :user_id LIMIT 1");
+    $getUserInfo->bindParam(':user_id', $user_id, PDO::PARAM_INT);
+    $getUserInfo->execute();
+
+    if($getUserInfo->rowCount() == 1) {
+        // User was found.
+        $User = $getUserInfo->fetch(PDO::FETCH_ASSOC);
+    } else {
+        // User is not signed in.
+        header("Location: /logout.php"); exit;
+    }
 ?>
 
 
@@ -19,12 +32,13 @@
 
     <title>Dashboard</title>
 
-    <base href="/Devfiles/4th_class/" />
+    <base href="/Devfiles/PHP-Login-System/" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.1.7/css/uikit.min.css" />
 </head>
 <body>
     <div class="uk-section uk-container">
-        Dashboard here; You are signed in as user: <?php echo $_SESSION['user_id']; ?>
+        <h2>Dashboard</h2>
+        <p>Hello <?php echo $User['email']; ?>, you registered at <?php echo $User['reg_time']; ?>  </p>
     </div>
     <?php require_once 'inc/footer.php'?>
 </body>
